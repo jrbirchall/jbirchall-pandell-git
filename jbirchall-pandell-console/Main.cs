aee2729013fa4a674584e705ea96df6c392bbfa1
jrbirchall <jrbirchall@gmail.com>
2013-02-11 20:37:54 -0700

    Added resources to demonstrate localising the usage string.

--- jbirchall-pandell-console/Main.cs
+++ jbirchall-pandell-console/Main.cs
@@ -3,10 +3,7 @@
 */ 
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Resources;
 using jbirchall_shuffle;
 
 namespace jbirchall_pandell_console
@@ -32,23 +29,38 @@ namespace jbirchall_pandell_console
         /// Main method.
         /// </summary>
         /// <param name="args">Command line arguments</param>
-        static unsafe int Main(string[] args)
+        static int Main(string[] args)
         {
             try
             {
                 int nCount = 10000;
 
-                if (args.Length > 0 && args[0] == "-h")
+                // This is fairly rudimentary argument parsing.
+                // If we need more sophisticated work (and depending upon the project requirements)
+                // we'd either:
+                //   1)  Implement our own (if we're into a closely licensed situation, for example)
+                //   2)  Buy one (if we do that sort of thing)
+                //   3)  Use something like NDesk.Options
+
+                // In general though, command line parsing in C# kinda sucks when compared to what's
+                // available in Python and the like.  If we were doing a lot of command line work,
+                // and had the tools available, I'd strongly consider something like that instead.
+
+                if (args.Length > 0 && args[0].Equals("-h"))
                 {
                     Console.Write(Usage());
                 }
-                else if (args.Length > 0)
+                else
                 {
-                    nCount = Int32.Parse(args[0]);
+                    if (args.Length > 0)
+                    {
+                        nCount = Int32.Parse(args[0]);
+                    }
+
+                    EmitShuffle(nCount);
                 }
-                FastShuffleArray out_array = new FastShuffleArray(nCount);
-                for (int i = 0; i < out_array.Size; i++)
-                    Console.WriteLine(out_array.Pointer[i]);
+
+                // By convention, zero exit codes indicates all ok.
                 return 0;
             }
             catch (Exception e)
@@ -61,21 +73,33 @@ namespace jbirchall_pandell_console
 
             }
 
+            // By convention, non-zero exit codes indicate faults.
             return 1;
         }
 
         /// <summary>
-        /// Retrieve the usage string.
+        /// Emits a shuffled array of nCount elements to stdout.
         /// </summary>
-        /// <returns></returns>
+        /// <param name="nCount">Number of elements to shuffle and emit.</param>
+        static unsafe void EmitShuffle(int nCount)
+        {
+            FastShuffleArray out_array = new FastShuffleArray(nCount);
+            for (int i = 0; i < out_array.Size; i++)
+                Console.WriteLine(out_array.Pointer[i]);
+        }
+
+        
+        /// <summary>
+        /// Retrieve the usage string in the user's language.
+        /// </summary>
+        /// <returns>A string in the current user's language.</returns>
+        /// <remarks>
+        /// This is a simple use case where we would consider whether to localise the output or not using an embedded resource.
+        /// </remarks>
         static string Usage()
         {
-            return @"
-Pandell application console implementation.  Emits a shuffled list of integers with each integer on a seperate line.
-Usage:  jbirchall-pandell-console  [-h] [# of items]
-    -h  Print help.
-";
-
+            ResourceManager rm = new ResourceManager("jbirchall_pandell_console.Properties.Resources", typeof(PandellApp).Assembly);
+            return rm.GetString("Usage");
         }
     }
 }
